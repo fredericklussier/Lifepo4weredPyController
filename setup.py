9fb43eefa588aca42c3ffb3e3aecfe8e9242d783
Frederick Lussier <frederuck.lussier@hotmail.com>
2017-10-18 09:15:17 -0400

    prepare setup

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Natural Language :: English',
